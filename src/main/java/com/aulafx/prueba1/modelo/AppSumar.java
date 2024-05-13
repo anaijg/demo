8684545de4ce171dd149b7e419798b798e21f558
anaijg <ana.jimenezgomez@educa.madrid.org>
2024-05-13 18:02:52 +0200

    Hecha la vista y lanzada la aplicación. Falta el controller

---
+++ src/main/java/com/aulafx/prueba1/modelo/AppSumar.java
@@ -0,0 +1,24 @@
+package com.aulafx.prueba1.modelo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+// este es el inicio de la aplicación (tiene el main)
+public class AppSumar extends Application { // no se toca
+    @Override
+    public void start(Stage stage) throws IOException { // esto no se cambia
+        FXMLLoader fxmlLoader = new FXMLLoader(AppSumar.class.getResource("/com/aulafx/prueba1/sumar-view.fxml")); // aquí cambiamos la ruta del fxml; lo demás no se toca
+        Scene scene = new Scene(fxmlLoader.load(), 300, 300); // definimos el tamaño que queremos, lo demás no cambia
+
+        stage.setScene(scene); // esto no se toca
+        stage.show(); // esto no se toca
+    }
+
+    public static void main(String[] args) { // esto no se cambia
+        launch();
+    }
+}
