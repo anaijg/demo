fc8bca3350eba1bfe09f0e407e22125371500bde
anaijg <ana.jimenezgomez@educa.madrid.org>
2024-05-15 17:56:26 +0200

    Haciendo el controller. No sale String.valueof para la suma

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,14 @@
-module com.example.demo {
+module com.aulafx.prueba1 {
+    // necesito estos módulos
     requires javafx.controls;
     requires javafx.fxml;
 
+    // cada módulo debe ser abierto (opens) y exportado (exports)
+    exports com.aulafx.prueba1.modelo;
+    opens com.aulafx.prueba1.modelo to javafx.fxml;
 
-    opens com.example.demo to javafx.fxml;
-    exports com.example.demo;
+
+
+    exports com.aulafx.prueba1.controlador;
+    opens com.aulafx.prueba1.controlador to javafx.fxml;
 }
